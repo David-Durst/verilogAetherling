aa80760b550f615d2aea9364c2e8bce96a816651
David Durst <davidbdurst@gmail.com>
2020-03-04 23:06:51 -0800

    starting with mul_8uint

---
+++ mul/mul_8uint.v
@@ -0,0 +1,19 @@
+module mul_uint8(
+	               input wire [7:0] I0,
+	               input wire [7:0] I1,
+	               output reg [7:0] O,
+	               input wire       clk
+	               )
+`ifdef VERILATOR
+   reg [7:0] reg0;
+   reg [7:0] reg1;
+   always @( posedge clk ) begin
+      reg0 <= I0 * I1;
+      reg1 <= reg0;
+      O <= reg1;
+   end
+`endif
+`ifndef VERILATOR
+   $display (" non-verilator mul_uint8 not implemented yet.");
+`endif
+endmodule

ed34a46e5ec2f7ae304b452faa0ba2669b6b0614
David Durst <davidbdurst@gmail.com>
2020-03-06 12:16:55 -0800

    setting all out high bits correctly, not generating ips if already exist

--- generate_muls.tcl
+++ generate_muls.tcl
@@ -1,40 +1,50 @@
 ## Integer Multiplier
-create_project -in_memory 
+if { [file isdirectory "$IP"] } {
+    # if the IP files exist, we already generated the IP, so we can just
+    # read the ip definition (.xci)
+    read_ip [glob "$IP/*/*.xci"]
+} else {
+    file mkdir $IP
+    create_project -in_memory 
 
-set moduleName mul_uint8
-set bitWidth 8
-set pipeStages 3
-create_ip -name mult_gen -vendor xilinx.com -library ip -version 12.0 -module_name $moduleName
-set_property -dict [list CONFIG.PortAType {Unsigned} CONFIG.PortAWidth "$bitWidth" CONFIG.PortBType {Unsigned} CONFIG.PortBWidth "$bitWidth" CONFIG.Multiplier_Construction {Use_Mults}  CONFIG.Use_Custom_Output_Width {true} CONFIG.OutputWidthHigh "$bitWidth" CONFIG.PipeStages "$pipeStages" CONFIG.ClockEnable {false}] [get_ips $moduleName] 
-
-set moduleName mul_int8
-create_ip -name mult_gen -vendor xilinx.com -library ip -version 12.0 -module_name $moduleName
-set_property -dict [list CONFIG.PortAWidth "$bitWidth" CONFIG.PortBWidth "$bitWidth" CONFIG.Multiplier_Construction {Use_Mults}  CONFIG.Use_Custom_Output_Width {true} CONFIG.OutputWidthHigh "$bitWidth" CONFIG.PipeStages "$pipeStages" CONFIG.ClockEnable {false}] [get_ips $moduleName] 
-
-
-
-set moduleName mul_uint16
-set bitWidth 16
-set pipeStages 3
-create_ip -name mult_gen -vendor xilinx.com -library ip -version 12.0 -module_name $moduleName
-set_property -dict [list CONFIG.PortAType {Unsigned} CONFIG.PortAWidth "$bitWidth" CONFIG.PortBType {Unsigned} CONFIG.PortBWidth "$bitWidth" CONFIG.Multiplier_Construction {Use_Mults}  CONFIG.Use_Custom_Output_Width {true} CONFIG.OutputWidthHigh "$bitWidth" CONFIG.PipeStages "$pipeStages" CONFIG.ClockEnable {false}] [get_ips $moduleName] 
-
-set moduleName mul_int16
-create_ip -name mult_gen -vendor xilinx.com -library ip -version 12.0 -module_name $moduleName
-set_property -dict [list CONFIG.PortAWidth "$bitWidth" CONFIG.PortBWidth "$bitWidth" CONFIG.Multiplier_Construction {Use_Mults}  CONFIG.Use_Custom_Output_Width {true} CONFIG.OutputWidthHigh "$bitWidth" CONFIG.PipeStages "$pipeStages" CONFIG.ClockEnable {false}] [get_ips $moduleName] 
+    set moduleName mul_uint8
+    set bitWidth 8
+    set outHighBit [expr $bitWidth - 1]
+    set pipeStages 3
+    create_ip -name mult_gen -vendor xilinx.com -library ip -version 12.0 -module_name $moduleName -dir $IP
+    set_property -dict [list CONFIG.PortAType {Unsigned} CONFIG.PortAWidth "$bitWidth" CONFIG.PortBType {Unsigned} CONFIG.PortBWidth "$bitWidth" CONFIG.Multiplier_Construction {Use_Mults}  CONFIG.Use_Custom_Output_Width {true} CONFIG.OutputWidthHigh "$outHighBit" CONFIG.PipeStages "$pipeStages" CONFIG.ClockEnable {false}] [get_ips $moduleName] 
+    
+    set moduleName mul_int8
+    create_ip -name mult_gen -vendor xilinx.com -library ip -version 12.0 -module_name $moduleName -dir $IP
+    set_property -dict [list CONFIG.PortAWidth "$bitWidth" CONFIG.PortBWidth "$bitWidth" CONFIG.Multiplier_Construction {Use_Mults}  CONFIG.Use_Custom_Output_Width {true} CONFIG.OutputWidthHigh "$outHighBit" CONFIG.PipeStages "$pipeStages" CONFIG.ClockEnable {false}] [get_ips $moduleName] 
 
 
 
-set moduleName mul_uint32
-set bitWidth 32
-set pipeStages 6
-create_ip -name mult_gen -vendor xilinx.com -library ip -version 12.0 -module_name $moduleName
-set_property -dict [list CONFIG.PortAType {Unsigned} CONFIG.PortAWidth "$bitWidth" CONFIG.PortBType {Unsigned} CONFIG.PortBWidth "$bitWidth" CONFIG.Multiplier_Construction {Use_Mults}  CONFIG.Use_Custom_Output_Width {true} CONFIG.OutputWidthHigh "$bitWidth" CONFIG.PipeStages "$pipeStages" CONFIG.ClockEnable {false}] [get_ips $moduleName] 
+    set moduleName mul_uint16
+    set bitWidth 16
+    set pipeStages 3
+    set outHighBit [expr $bitWidth - 1]
+    create_ip -name mult_gen -vendor xilinx.com -library ip -version 12.0 -module_name $moduleName -dir $IP
+    set_property -dict [list CONFIG.PortAType {Unsigned} CONFIG.PortAWidth "$bitWidth" CONFIG.PortBType {Unsigned} CONFIG.PortBWidth "$bitWidth" CONFIG.Multiplier_Construction {Use_Mults}  CONFIG.Use_Custom_Output_Width {true} CONFIG.OutputWidthHigh "$outHighBit" CONFIG.PipeStages "$pipeStages" CONFIG.ClockEnable {false}] [get_ips $moduleName] 
 
-set moduleName mul_int32
-create_ip -name mult_gen -vendor xilinx.com -library ip -version 12.0 -module_name $moduleName
-set_property -dict [list CONFIG.PortAWidth "$bitWidth" CONFIG.PortBWidth "$bitWidth" CONFIG.Multiplier_Construction {Use_Mults}  CONFIG.Use_Custom_Output_Width {true} CONFIG.OutputWidthHigh "$bitWidth" CONFIG.PipeStages "$pipeStages" CONFIG.ClockEnable {false}] [get_ips $moduleName] 
+    set moduleName mul_int16
+    create_ip -name mult_gen -vendor xilinx.com -library ip -version 12.0 -module_name $moduleName -dir $IP
+    set_property -dict [list CONFIG.PortAWidth "$bitWidth" CONFIG.PortBWidth "$bitWidth" CONFIG.Multiplier_Construction {Use_Mults}  CONFIG.Use_Custom_Output_Width {true} CONFIG.OutputWidthHigh "$outHighBit" CONFIG.PipeStages "$pipeStages" CONFIG.ClockEnable {false}] [get_ips $moduleName] 
 
-generate_target {all} [get_ips]
 
-synth_ip [get_ips]
+
+    set moduleName mul_uint32
+    set bitWidth 32
+    set pipeStages 6
+    set outHighBit [expr $bitWidth - 1]
+    create_ip -name mult_gen -vendor xilinx.com -library ip -version 12.0 -module_name $moduleName -dir $IP
+    set_property -dict [list CONFIG.PortAType {Unsigned} CONFIG.PortAWidth "$bitWidth" CONFIG.PortBType {Unsigned} CONFIG.PortBWidth "$bitWidth" CONFIG.Multiplier_Construction {Use_Mults}  CONFIG.Use_Custom_Output_Width {true} CONFIG.OutputWidthHigh "$outHighBit" CONFIG.PipeStages "$pipeStages" CONFIG.ClockEnable {false}] [get_ips $moduleName] 
+
+    set moduleName mul_int32
+    create_ip -name mult_gen -vendor xilinx.com -library ip -version 12.0 -module_name $moduleName -dir $IP
+    set_property -dict [list CONFIG.PortAWidth "$bitWidth" CONFIG.PortBWidth "$bitWidth" CONFIG.Multiplier_Construction {Use_Mults}  CONFIG.Use_Custom_Output_Width {true} CONFIG.OutputWidthHigh "$outHighBit" CONFIG.PipeStages "$pipeStages" CONFIG.ClockEnable {false}] [get_ips $moduleName] 
+
+    generate_target {all} [get_ips]
+
+    synth_ip [get_ips]
+}

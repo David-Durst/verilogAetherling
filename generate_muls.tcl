cd6e3c99f4f78e098220611fbb33e66e1c349219
David Durst <davidbdurst@gmail.com>
2020-03-06 01:56:11 -0800

    opening projcet when generatingip

--- generate_muls.tcl
+++ generate_muls.tcl
@@ -1,16 +1,15 @@
 ## Integer Multiplier
+create_project -in_memory 
 
 set moduleName mul_uint8
 set bitWidth 8
 set pipeStages 3
 create_ip -name mult_gen -vendor xilinx.com -library ip -version 12.0 -module_name $moduleName
 set_property -dict [list CONFIG.PortAType {Unsigned} CONFIG.PortAWidth {$bitWidth} CONFIG.PortBType {Unsigned} CONFIG.PortBWidth {$bitWidth} CONFIG.Multiplier_Construction {Use_Mults}  CONFIG.Use_Custom_Output_Width {true} CONFIG.OutputWidthHigh {$bitWidth} CONFIG.PipeStages {$pipeStages} CONFIG.ClockEnable {false}] [get_ips $moduleName] 
-generate_target {all} [get_ips $moduleName]
 
 set moduleName mul_int8
 create_ip -name mult_gen -vendor xilinx.com -library ip -version 12.0 -module_name $moduleName
 set_property -dict [list CONFIG.PortAWidth {$bitWidth} CONFIG.PortBWidth {$bitWidth} CONFIG.Multiplier_Construction {Use_Mults}  CONFIG.Use_Custom_Output_Width {true} CONFIG.OutputWidthHigh {$bitWidth} CONFIG.PipeStages {$pipeStages} CONFIG.ClockEnable {false}] [get_ips $moduleName] 
-generate_target {all} [get_ips $moduleName]
 
 
 
@@ -19,12 +18,10 @@ set bitWidth 16
 set pipeStages 3
 create_ip -name mult_gen -vendor xilinx.com -library ip -version 12.0 -module_name $moduleName
 set_property -dict [list CONFIG.PortAType {Unsigned} CONFIG.PortAWidth {$bitWidth} CONFIG.PortBType {Unsigned} CONFIG.PortBWidth {$bitWidth} CONFIG.Multiplier_Construction {Use_Mults}  CONFIG.Use_Custom_Output_Width {true} CONFIG.OutputWidthHigh {$bitWidth} CONFIG.PipeStages {$pipeStages} CONFIG.ClockEnable {false}] [get_ips $moduleName] 
-generate_target {all} [get_ips $moduleName]
 
 set moduleName mul_int16
 create_ip -name mult_gen -vendor xilinx.com -library ip -version 12.0 -module_name $moduleName
 set_property -dict [list CONFIG.PortAWidth {$bitWidth} CONFIG.PortBWidth {$bitWidth} CONFIG.Multiplier_Construction {Use_Mults}  CONFIG.Use_Custom_Output_Width {true} CONFIG.OutputWidthHigh {$bitWidth} CONFIG.PipeStages {$pipeStages} CONFIG.ClockEnable {false}] [get_ips $moduleName] 
-generate_target {all} [get_ips $moduleName]
 
 
 
@@ -33,9 +30,11 @@ set bitWidth 32
 set pipeStages 6
 create_ip -name mult_gen -vendor xilinx.com -library ip -version 12.0 -module_name $moduleName
 set_property -dict [list CONFIG.PortAType {Unsigned} CONFIG.PortAWidth {$bitWidth} CONFIG.PortBType {Unsigned} CONFIG.PortBWidth {$bitWidth} CONFIG.Multiplier_Construction {Use_Mults}  CONFIG.Use_Custom_Output_Width {true} CONFIG.OutputWidthHigh {$bitWidth} CONFIG.PipeStages {$pipeStages} CONFIG.ClockEnable {false}] [get_ips $moduleName] 
-generate_target {all} [get_ips $moduleName]
 
 set moduleName mul_int32
 create_ip -name mult_gen -vendor xilinx.com -library ip -version 12.0 -module_name $moduleName
 set_property -dict [list CONFIG.PortAWidth {$bitWidth} CONFIG.PortBWidth {$bitWidth} CONFIG.Multiplier_Construction {Use_Mults}  CONFIG.Use_Custom_Output_Width {true} CONFIG.OutputWidthHigh {$bitWidth} CONFIG.PipeStages {$pipeStages} CONFIG.ClockEnable {false}] [get_ips $moduleName] 
-generate_target {all} [get_ips $moduleName]
+
+generate_target {all} [get_ips]
+
+# synth_ip [get_ips]
